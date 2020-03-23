233c958ee9efe12510f68d613567cebcd2da2a66
majadc <maja.chabros@gmail.com>
2020-03-23 09:47:03 +0100

    changed class is flipped

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let travelCardsItems = document.getElementsByClassName('travel-cards__scene-flip
 for ( let item of travelCardsItems ) {
 	var handler = function() {
 		console.log(item);
-		item.classList.toggle('is-flipped');
+		item.classList.toggle('travel-cards__tiem--is-flipped');
 	
 	};
 	item.addEventListener('click', handler);
