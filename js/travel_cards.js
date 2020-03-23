f55d4947c4a159fac687016f68f411d974b3ce3a
majadc <maja.chabros@gmail.com>
2020-03-23 20:48:07 +0100

    adding scale effect on flipped cards

--- js/travel_cards.js
+++ js/travel_cards.js
@@ -1,42 +1,22 @@
 let travelCardsItems = document.getElementsByClassName('travel-cards__scene-flip');
-for ( var i of travelCardsItems ) {
-	i.active = false;
-	
-}
 
 for ( let item of travelCardsItems ) {
-	var handler = function() {
-		
-		
-		if ( item.classList.contains('travel-cards__item--is-inactive') ) {
-			item.classList.remove('travel-cards__item--is-inactive');
-			console.log('if1');
-		}
-		
-		
-		if ( item.classList.contains('travel-cards__tiem--is-flipped') ) {	
-			item.active = false;
-			console.log(i);
-		} else {
-			item.active = true;
-		
-		}
-		item.classList.toggle('travel-cards__tiem--is-flipped');
 
-			let travelCardsItemsInactive = document.getElementsByClassName('travel-cards__scene-flip');
-			
+	var handler = function() {
+		if (item.classList.contains('travel-cards__tiem--is-flipped') )  {
+			item.classList.remove('travel-cards__tiem--is-inactive');
+			item.classList.remove('travel-cards__tiem--is-flipped');
+		} 
+		else {
+			item.classList.add('travel-cards__tiem--is-flipped');
+			let travelCardsItemsInactive = document.getElementsByClassName('travel-cards__tiem--is-flipped');
 			for ( let card of  travelCardsItemsInactive ) {
-				//console.log(card);
-				if ( card.classList.contains('travel-cards__tiem--is-flipped') && card.active === false   ) {
-					card.classList.add('travel-cards__item--is-inactive');
-					console.log('card');
-					
+				if (card.classList.contains('travel-cards__tiem--is-flipped') && item != card) {
+					card.classList.add('travel-cards__tiem--is-inactive');
 				}
+
 			}
-			
-		
-		
-	
+		}
 	};
 	item.addEventListener('click', handler);
 }//for
